041687a521d6347553d189585c12a3291b78040d
Kristex95 <kristex95@gmail.com>
2023-09-01 01:52:53 +0300

    first commit. Sudoku generation - done

---
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.sudoku
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.GridLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sudoku.ui.theme.SudokuTheme
+
+const val TAG = "Andrii"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gridLayout = findViewById<GridLayout>(R.id.gridLayout)
+
+        for (row in 0 until 9) {
+            for (col in 0 until 9) {
+                val buttonLayout = LayoutInflater.from(this)
+                    .inflate(R.layout.button_grid_item, gridLayout, false)
+
+                val button = buttonLayout.findViewById<Button>(R.id.button)
+                button.text = "$row-$col" // Set button text as per your requirement
+                button.tag = "$row-$col" // Set a tag if needed
+
+                // Add click listener to the button
+                button.setOnClickListener { view ->
+                    val tag = view.tag as String
+                    // Handle button click here
+                }
+
+                // Add the button to the GridLayout
+                gridLayout.addView(buttonLayout)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sudoku/SudokuMain.kt
@@ -0,0 +1,112 @@
+package com.example.sudoku
+
+import android.util.Log
+import kotlin.random.Random
+
+object SudokuMain {
+    private const val sheetLength = 9
+    private var answerSheet = CustomArrayList<CustomArrayList<Int>>()
+
+    init{
+        CreateSheet()
+    }
+
+    private fun CreateSheet(){
+        val oneToNine = (1..9).toMutableList()
+        var list = CustomArrayList<Int>()
+        list.addAll(oneToNine)
+
+        answerSheet.add(list)
+        for (i in 1..2){
+            list = list.clone()
+            list.shiftLeft(3)
+            answerSheet.add(list)
+        }
+
+        var copySheet = answerSheet.clone()
+        for (i in copySheet){
+            list = i.clone()
+            list.shiftLeft(1)
+            answerSheet.add(list)
+        }
+        for (i in copySheet){
+            list = i.clone()
+            list.shiftLeft(2)
+            answerSheet.add(list)
+        }
+        var randomSwapCount = Random.nextInt(20, 40)
+
+        for(i in 0..randomSwapCount){
+            swapRandomCols()
+            swapRandomRows()
+            if(i % 2 == 0){
+                answerSheet = transpose()
+            }
+        }
+    }
+
+    private fun transpose(): CustomArrayList<CustomArrayList<Int>> {
+        var newAnswerSheet = CustomArrayList<CustomArrayList<Int>>()
+        for(i in 0 until answerSheet.size)
+            newAnswerSheet.add(CustomArrayList())
+
+        for(i in 0 until answerSheet.size){
+            for(j in 0 until answerSheet.size){
+                newAnswerSheet[j].add(answerSheet[i][j])
+            }
+        }
+        return newAnswerSheet
+    }
+
+    private fun swapRandomRows(){
+        val firstRandom = Random.nextInt(6,9)
+        var secondRandom = Random.nextInt(6,9)
+        while(firstRandom == secondRandom){
+            secondRandom = Random.nextInt(6,9)
+        }
+        val rowCoef = Random.nextInt(0,3)
+
+        val firstRowIndex = firstRandom - (rowCoef * 3)
+        val secondRowIndex = secondRandom - (rowCoef * 3)
+
+        var firstRow = answerSheet[firstRowIndex].clone()
+        answerSheet[firstRowIndex] = answerSheet[secondRowIndex].clone()
+        answerSheet[secondRowIndex]=firstRow
+
+    }
+    private fun swapRandomCols(){
+        val firstRandom = Random.nextInt(6,9)
+        var secondRandom = Random.nextInt(6,9)
+        while(firstRandom == secondRandom){
+            secondRandom = Random.nextInt(6,9)
+        }
+        val colCoef = Random.nextInt(0,3)
+
+        val firstColIndex = firstRandom - (colCoef * 3)
+        val secondColIndex = secondRandom - (colCoef * 3)
+
+        for (i in 0 until 9){
+            var firstCol = answerSheet[i][firstColIndex]
+            answerSheet[i][firstColIndex]= answerSheet[i][secondColIndex]
+            answerSheet[i][secondColIndex]=firstCol
+        }
+    }
+
+    fun LogAnswerSheet(){
+
+        for(row in 0 until answerSheet.size){
+            var rowStr = ""
+            if(row % 3 == 0) {
+                Log.d("Kristex", " ")
+            }
+            for (value in 0 until  answerSheet.size) {
+                if(value % 3 == 0 ){
+                    rowStr += "  "
+                }
+                rowStr += answerSheet[row][value].toString() + " "
+
+            }
+            Log.d("Kristex", rowStr)
+        }
+    }
+}
