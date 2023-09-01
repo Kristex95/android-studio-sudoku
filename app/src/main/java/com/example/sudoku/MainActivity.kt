69cfb84c03c284c32278276b54a378a6768f5a6c
gr1sus <marmokmarin@gmail.com>
2023-09-02 00:09:36 +0300

    colored button

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sudoku
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -15,54 +17,72 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.sudoku.ui.theme.SudokuTheme
 
 const val TAG = "Andrii"
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("ResourceAsColor")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         SudokuMain.LogAnswerSheet()
-
+        val color1 = R.color.light_wight
+        val color2 = R.color.pumpkin
+        var currentColor = color1
 
 
         val gridLayout = findViewById<GridLayout>(R.id.gridLayout)
         var answerSheet = SudokuMain.answerSheet
-
-
+        var paintRow=true
+        var paintCol=true
         for (row in 0 until 9) {
-            for (col in 0 until 9) {
-                val buttonLayout = LayoutInflater.from(this)
-                    .inflate(R.layout.button_grid_item, gridLayout, false)
+            if(row%3==0)paintRow=!paintRow
+            paintCol=true
+                for (col in 0 until 9) {
+                    if (col%3==0)paintCol=!paintCol
+                    if (paintRow != paintCol)currentColor=color1 else currentColor=color2
+                    val buttonLayout = LayoutInflater.from(this)
+                        .inflate(R.layout.button_grid_item, gridLayout, false)
 
-                val button = buttonLayout.findViewById<Button>(R.id.button)
-                var buttText = ""
-
-                buttText+=answerSheet[row][col].toString()
-                Log.d("Kristex", buttText)
-                button.tag=buttText
-                button.text ="$buttText"  // Set button text as per your requirement
-                buttText=""
-
+                    val button = buttonLayout.findViewById<Button>(R.id.button)
+                    var buttText = ""
+                    button.setBackgroundColor(R.color.black)
 
 
-                button.setOnClickListener {
+                    button.backgroundTintList = ColorStateList.valueOf(
+                        ContextCompat.getColor(this, currentColor)
+                    )
+                    buttText += answerSheet[row][col].toString()
+                    Log.d("Kristex", buttText)
+                    button.tag = buttText
+                    button.text = "$buttText"  // Set button text as per your requirement
+                    buttText = ""
 
-                    val buttonText = (it as Button).text.toString()
-                    var number = buttonText.toInt()
 
 
-                    number = (number + 1) % 10
+                    button.setOnClickListener {
+
+                        val buttonText = (it as Button).text.toString()
+                        var number = buttonText.toInt()
 
 
-                    button.text = number.toString()
-                    Toast.makeText(this, "Вы нажали кнопку $buttonText", Toast.LENGTH_SHORT).show()
+                        number = (number + 1) % 10
+
+
+                        button.text = number.toString()
+                        Toast.makeText(this, "Вы нажали кнопку $buttonText", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                    currentColor = if (currentColor == color1) color2 else color1
+                    // Add the button to the GridLayout
+                    gridLayout.addView(buttonLayout)
                 }
-                // Add the button to the GridLayout
-                gridLayout.addView(buttonLayout)
-            }
         }
     }
 }
